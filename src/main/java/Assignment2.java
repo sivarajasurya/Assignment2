45dfe60b990439cd1735d067b8c044464c402b70
sivasuryasurya <sivarajasurya.elangovan@gcitsolutions.com>
2022-09-27 15:15:18 +0530

    added edit function

--- src/main/java/Assignment2.java
+++ src/main/java/Assignment2.java
@@ -8,15 +8,18 @@ public class Assignment2 {
 
     // 1. Enter Person Data
     // 2. Read Person Data
-    // 3. Exit
+    // 3. To edit existing data
+    // 4. Exit
     public void detail() throws Exception {
 
 
-        System.out.println("Enter  1 for  Enter Person Data \n Enter 2 for  Read Person Data  \n Enter 3 for quit ");
+        System.out.println(" Enter  1 for  Enter Person Data \n Enter 2 for  Read Person Data \n Enter 3 to edit existing data \n Enter 4 for quit \n Enter your value");
+
         menu = get.nextInt();
 
+        if(menu>0 && menu<5)
+        {
         switch (menu) {
-
             //Enter Person Data
             case 1: {
                 System.out.println("Enter name , Age , Sex");
@@ -27,7 +30,6 @@ public class Assignment2 {
                 detail();
                 break;
             }
-
 
             //Read Person Data
             case 2: {
@@ -42,20 +44,31 @@ public class Assignment2 {
                 int age = get.nextInt();
 
                 //Get file name as "<name><age>.xml"
-                obj.personRead(name + age + ".xml");
+                System.out.println(obj.personRead(name + age + ".xml"));
+
                 detail();
+                break;
+            }
+
+            case 3: {
+                Person obj = new Person();
+                System.out.println("Enter the name of the .xml file that you want to edit \n ");
+                obj.edit(get.next());
+
+
+                detail();
+                break;
 
             }
-            break;
-
-
             //Exit
-            case 3: {
+            case 4: {
                 get.close();
                 System.exit(0);
             }
-
-
+        }
+        }else {
+            System.out.println("Incorrect value entered ");
+            detail();
         }
     }
 
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,6 +1,7 @@
 import org.w3c.dom.Document;
 import java.io.*;
 import java.io.FileOutputStream;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.bind.annotation.XmlElement;
@@ -15,28 +16,17 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-
-
-
 @XmlRootElement
 public class Person{
 
-
-
-
-
 //    @XmlAttribute
     private String Name;
-
     private int Age;
-
     private char Sex;
-
-    public Person(){
-
+    public Person() throws JAXBException {
     }
 
-    public Person(String name, int age, char sex) {
+    public Person(String name, int age, char sex) throws JAXBException {
         super();
         this.Name = name;
         this.Age = age;
@@ -47,7 +37,6 @@ public class Person{
     public String getName() {
         return Name;
     }
-
     public void setName(String name) {
         this.Name = name;
     }
@@ -56,7 +45,6 @@ public class Person{
     public int getAge() {
         return Age;
     }
-
     public void setAge(int age) {
         this.Age = age;
     }
@@ -65,31 +53,37 @@ public class Person{
     public char getSex() {
         return Sex;
     }
-
     public void setSex(char sex) {
         this.Sex = sex;
     }
-
 
     @Override
     public String toString(){
         return "Person [Name = "+ this.Name+ " , Age = "+ this.Age +" , sex = " +this.Sex+"]";
     }
+    static JAXBContext jaxbContext;
+
+    static {
+        try {
+            jaxbContext = JAXBContext.newInstance(Person.class);
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static Scanner get = new Scanner(System.in);
 
     //Serilize the object as XML and write to the file.
     //And print the .xml file
     public String write(String xmlFileName) throws Exception{
 
-
         try {
 
-            JAXBContext contextobj = JAXBContext.newInstance(Person.class);
-            Marshaller marshelling = contextobj.createMarshaller();
-            marshelling.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
+            Marshaller marshelling = jaxbContext.createMarshaller();
+            marshelling.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             marshelling.marshal(this, new FileOutputStream(xmlFileName));
             System.out.println("employee.xml is created successfully");
-
 
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
@@ -98,6 +92,8 @@ public class Person{
             Writer out = new StringWriter();
             tf.transform(new DOMSource(doc), new StreamResult(out));
             System.out.println(out.toString());
+
+
         }
         catch (Exception e){
 
@@ -106,38 +102,64 @@ public class Person{
             Logger logger = Logger.getAnonymousLogger();
             //Exception e1 = new Exception(e);
             logger.log(Level.SEVERE, "an exception was thrown", e);
-
             System.out.println(e);
         }
-
-
         return "successfully saved to filename";
     }
 
-
-
     //De-serilize the XML and return the Person object.
-    public void personRead(String xmlFilePath) throws Exception{
+    public static Person personRead(String xmlFilePath) throws Exception{
 
 
-
-       try {
+      try {
             File file = new File(xmlFilePath);
-          JAXBContext jaxbContext = JAXBContext.newInstance(Person.class);
 
           Unmarshaller jabUnmarshaller = jaxbContext.createUnmarshaller();
           Person p2 = (Person) jabUnmarshaller.unmarshal(file);
-           System.out.println("Name = "+p2.getName()+" Age = "+ p2.getAge()+" Sex = "+p2.getSex());
+
+        return p2;
        } catch (Exception e){
 
            //Log and Throw appropriate exception on error
-
            Logger logger = Logger.getAnonymousLogger();
-           Exception e1 = new Exception(e);
-           logger.log(Level.SEVERE, "an exception was thrown", e1);
-           System.out.println(e1);
+           logger.log(Level.SEVERE, "an exception was thrown", e);
+           System.out.println(e);
 
        }
+    return null;
+    }
+
+
+    public void edit(String xmlFilePath) throws Exception {
+
+        File file = new File(xmlFilePath);
+
+        Unmarshaller jabUnmarshaller = jaxbContext.createUnmarshaller();
+        Person p3 = (Person) jabUnmarshaller.unmarshal(file);
+
+
+        this.Name = p3.getName();
+        this.Age = p3.getAge();
+        this.Sex = p3.getSex();
+
+
+        Marshaller marshelling = jaxbContext.createMarshaller();
+        marshelling.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshelling.marshal(this, new FileOutputStream(xmlFilePath));
+        System.out.println(p3.getName()+p3.getAge()+".xml is created successfully");
+
+        File rename = new File(p3.getName()+p3.getAge()+".xml");
+        file.renameTo(rename);
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new FileInputStream(rename));
+        Transformer tf = TransformerFactory.newInstance().newTransformer();
+        Writer out = new StringWriter();
+        tf.transform(new DOMSource(doc), new StreamResult(out));
+        System.out.println(out.toString());
 
     }
+
+
 }
